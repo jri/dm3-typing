24048c6eb5c908ce1af6cb3a5a1fde75a5071d8a
Jörg Richter <jri@deepamehta.de>
2010-01-20 01:54:04 +0100

    1) Adapted to current DeepaMehta 3 version. 2) Adds a new topic type "Topic Type". That is, types can now be created interactively. ** Not yet complete ** No field definition yet.

--- vendor/_attachments/script/dm3_typing.js
+++ vendor/_attachments/script/dm3_typing.js
@@ -1,5 +1,25 @@
 function dm3_typing() {
 
+    doctype_implementation("vendor/dm3-typing/script/topictype_document.js")
+
+    // IMPORTANT: if you make changes on the Topic Type declaration, you
+    // must change deepamehta3.js accordingly (method "type_template").
+    add_topic_type("Topic Type", {
+        fields: [
+            {id: "type-id", model: {type: "text"}, view: {editor: "single line", label: "Type ID"}, content: ""}
+        ],
+        view: {},
+        implementation: "TopictypeDocument"
+    })
+
+
+
+    /**************************************************************************************************/
+    /**************************************** Overriding Hooks ****************************************/
+    /**************************************************************************************************/
+
+
+
     this.init = function() {
         $("#searchmode_select").append($("<option>").text("By Type"))
     }
